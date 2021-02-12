2b3a8b6c5b7ded44a678fd0946eb28dc09906076
Harrison King <authorharrisonking@gmail.com>
2021-02-12 14:14:50 -0700

    added body-parser

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+// setup body-parser.
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -23,6 +27,16 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+//
+app.get('/api/whoami', (req, res) => {
+
+  res.json({
+    ipaddress: '',
+    language: '',
+    software: ''
+  });
 });
 
 
